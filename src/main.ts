6a6885a0b4073b1f953ddabdfe2c994a87a2fee8
mustafahashemi577 <mustafahashemi577@gmail.com>
2026-04-17 06:30:33 +0430

    Enable cros to connect with front

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
