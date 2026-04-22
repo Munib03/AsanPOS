e2f883da4eefe7699a38e3d39e040423cebcedd6
Hedayatullah Yamin <h.yamin000@gmail.com>
2026-04-22 13:21:23 +0430

    Store Controller Updated

--- src/stores/stores.controller.ts
+++ src/stores/stores.controller.ts
@@ -8,13 +8,11 @@ import { JwtAuthGuard } from '../shared/jwt/jwt-auth.guard';
 export class StoresController {
   constructor(private readonly storesService: StoresService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.storesService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.storesService.findOne(id);
