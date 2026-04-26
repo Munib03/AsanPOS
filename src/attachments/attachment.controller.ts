6653d5c029ed28063066268394b97ac6d0b7ef9b
mustafahashemi577 <mustafahashemi577@gmail.com>
2026-04-26 11:45:14 +0430

    endpoints

--- src/attachments/attachment.controller.ts
+++ src/attachments/attachment.controller.ts
@@ -1,27 +1,31 @@
-import { Controller, Post, Delete, Get, Param, UploadedFile, UseInterceptors, UseGuards, Put } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Delete,
+  Get,
+  Param,
+  UploadedFile,
+  UseInterceptors,
+  UseGuards,
+  Put,
+} from '@nestjs/common';
 import { AttachmentService } from './attachment.service';
 import { JwtAuthGuard } from '../shared/jwt/jwt-auth.guard';
 import { ImageUploadInterceptor } from '../shared/interceptors/image-upload.interceptor';
 import { CurrentUser } from '../shared/decorators/current-user.decorator';
-
 
 @Controller('attachments')
 @UseGuards(JwtAuthGuard)
 export class AttachmentController {
   constructor(private readonly attachmentService: AttachmentService) {}
 
-
-  @Put("img-update")
+  @Put('img')
   @UseInterceptors(ImageUploadInterceptor)
-  uploadImage(
-    @CurrentUser() user: { id: string },
-    @UploadedFile() file: any,
-  ) {
+  uploadImage(@CurrentUser() user: { id: string }, @UploadedFile() file: any) {
     return this.attachmentService.uploadImage(user.id, file);
   }
 
-
-  @Delete("img-delete")
+  @Delete('img')
   removeImage(@CurrentUser() user: { id: string }) {
     return this.attachmentService.removeImage(user.id);
   }
