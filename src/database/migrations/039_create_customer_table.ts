bd0d5db33213a99bc701466651af1dc821967e18
Hedayatullah Yamin <h.yamin000@gmail.com>
2026-05-11 15:48:04 +0430

    Updates to db

--- src/database/migrations/039_create_customer_table.ts
+++ src/database/migrations/039_create_customer_table.ts
@@ -8,8 +8,7 @@ export async function up(knex: Knex): Promise<void> {
             table.uuid("id").primary().notNullable();
             table.string("name").notNullable();
             table.string("address").notNullable();
-            // table.timestamp('created_at').defaultTo(knex.fn.now());
-            // table.timestamp('updated_at').defaultTo('now()');
+            table.string("phone").nullable();
         });
     }
 }
