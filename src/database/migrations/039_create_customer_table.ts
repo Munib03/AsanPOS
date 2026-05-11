52a1c57992c68c1859dbcc30669d5484456aa4ef
Hedayatullah Yamin <h.yamin000@gmail.com>
2026-05-11 11:48:53 +0430

    Updates to customer table.

--- src/database/migrations/039_create_customer_table.ts
+++ src/database/migrations/039_create_customer_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
     const exists = await knex.schema.hasTable('customer');
 
     if (!exists) {
-        await knex.schema.createTable("cutomer", (table: Knex.TableBuilder) => {
+        await knex.schema.createTable("customer", (table: Knex.TableBuilder) => {
             table.uuid("id").primary().notNullable();
             table.string("name").notNullable();
             table.string("address").notNullable();
